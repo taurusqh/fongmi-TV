11597bbeb239f6ba5f6c0211658ec7c33ec2bf42
taurusqh <35243660+taurusqh@users.noreply.github.com>
2026-05-15 13:06:50 +0800

    Delete list-zip.ps1
    Signed-off-by: taurusqh <35243660+taurusqh@users.noreply.github.com>

--- list-zip.ps1
+++
@@ -1,4 +0,0 @@
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-$z = [System.IO.Compression.ZipFile]::OpenRead('D:/Android/TVBox/fongmi-TV/downloads/mobile-arm64-v8a.zip')
-$z.Entries | ForEach-Object { $_.Name }
-$z.Dispose()
